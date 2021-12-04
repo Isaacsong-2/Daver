96224969534f41eea52fb9f6d34a3e5950606f54
Isak Song <thddltkr10@naver.com>
2021-12-04 19:45:23 +0900

    제목,글쓴이,작성일,조회수 스크래핑

--- daver/mailview/crawling.py
+++ daver/mailview/crawling.py
@@ -5,11 +5,36 @@ url = "https://gall.dcinside.com/board/lists/?id=gongik_new"
 header = {'User-agent': '*'}
 html = requests.get(url, headers=header)
 soup = bs(html.content, "html.parser")
-# for child in soup.em.children:
-# print(child)
-글 = soup.find(class_="ub-content us-post")
-제목 = 글.find(class_="gall_tit ub-word").get_text()
-# print(글)
-print(제목)
-# for x in range(0, 1):
-#     print(soup.select(".gall_tit ub-word").get_text())
+
+# 제목
+제목 = soup.find_all(class_="gall_tit ub-word")
+title = []
+for q in 제목:
+    title.append(q.get_text())
+print(title)
+# 글쓴이
+제목 = soup.find_all(class_="nickname")
+writer = []
+for q in 제목:
+    writer.append(q.get_text())
+print(writer)
+# 작성일
+cnt = 0
+작성일 = soup.find_all(class_="gall_date")
+date = []
+for q in 작성일:
+    if cnt < 2:
+        cnt += 1
+    else:
+        date.append(q.get_text())
+print(date)
+# 조회수
+cnt = 0
+조회수 = soup.find_all(class_="gall_count")
+count = []
+for q in 조회수:
+    if cnt < 2:
+        cnt += 1
+    else:
+        count.append(q.get_text())
+print(count)
