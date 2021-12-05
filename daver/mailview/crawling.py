310bbd74084004064b8d04cdbe302783c4a35b66
Isak Song <thddltkr10@naver.com>
2021-12-05 16:58:33 +0900

    crawling데이터 dictionary로 변환

--- daver/mailview/crawling.py
+++ daver/mailview/crawling.py
@@ -1,48 +1,62 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import re
-url = "https://gall.dcinside.com/board/lists/?id=gongik_new"
-header = {'User-agent': '*'}
-html = requests.get(url, headers=header)
-soup = bs(html.content, "html.parser")
+import os
 
-# 제목
-cnt = 0
-제목 = soup.find_all(class_="gall_tit ub-word")
-title = []
-for q in 제목:
-    if cnt < 2:
-        cnt += 1
-    else:
-        if q.get_text()[-2] == ']':
-            title.append(q.get_text()[1:-5])
+
+def crawl():
+    url = "https://gall.dcinside.com/board/lists/?id=gongik_new"
+    header = {'User-agent': '*'}
+    html = requests.get(url, headers=header)
+    soup = bs(html.content, "html.parser")
+
+    # 제목
+    cnt = 0
+    제목 = soup.find_all(class_="gall_tit ub-word")
+    title = []
+    for q in 제목:
+        if cnt < 2:
+            cnt += 1
         else:
-            title.append(q.get_text()[1:-2])
-print(title)
-# 글쓴이
-제목 = soup.find_all(class_="nickname")
-writer = []
-for q in 제목:
-    writer.append(q.get_text())
-print(writer)
-# 작성일
-cnt = 0
-작성일 = soup.find_all(class_="gall_date")
-date = []
-for q in 작성일:
-    if cnt < 2:
-        cnt += 1
-    else:
-        date.append(q.get_text())
-print(date)
-# 조회수
-cnt = 0
-조회수 = soup.find_all(class_="gall_count")
-count = []
-for q in 조회수:
-    if cnt < 2:
-        cnt += 1
-    else:
-        count.append(q.get_text())
-print(count)
-print(len(title), len(writer), len(date), len(count))
+            if q.get_text()[-2] == ']':
+                # db['title'] = q.get_text()[1:-5]
+                title.append(q.get_text()[1:-5])
+            else:
+                # db['title'] = q.get_text()[1:-2]
+                title.append(q.get_text()[1:-2])
+    # print(title)
+    # 글쓴이
+    글쓴이 = soup.find_all(class_="nickname")
+    writer = []
+    for q in 글쓴이:
+        # db['writer'] = q.get_text()
+        writer.append(q.get_text())
+    # print(writer)
+    # 작성일
+    cnt = 0
+    작성일 = soup.find_all(class_="gall_date")
+    date = []
+    for q in 작성일:
+        if cnt < 2:
+            cnt += 1
+        else:
+            # db['date'] = q.get_text()
+            date.append(q.get_text())
+    # print(date)
+    # 조회수
+    cnt = 0
+    조회수 = soup.find_all(class_="gall_count")
+    count = []
+    for q in 조회수:
+        if cnt < 2:
+            cnt += 1
+        else:
+            # db['count'] = q.get_text()
+            count.append(q.get_text())
+    # print(count)
+    db = {}
+    db['title'] = title
+    db['writer'] = writer
+    db['date'] = date
+    db['count'] = count
+    return db
+# print(len(title), len(writer), len(date), len(count))
--- daver/mailview/views.py
+++ daver/mailview/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .crawling import crawl
 
 
 def index(request):
-    return render(request, '1.html')
+    db = crawl()
+    # writer = crawling.writer
+    # date = crawling.date
+    # count = crawling.count
+    print(db)
+    return render(request, '1.html', db)
