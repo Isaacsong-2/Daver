1199b995d69ad88da30552f50ac3541f878d8f86
Isak Song <thddltkr10@naver.com>
2021-12-07 20:08:02 +0900

    mail_main에서 detail로 가는 링크 생성

--- daver/mailview/crawling.py
+++ daver/mailview/crawling.py
@@ -10,57 +10,52 @@ def crawl():
     soup = bs(html.content, "html.parser")
 
     # 제목
-    cnt = 0
     제목 = soup.find_all(class_="gall_tit ub-word")
     title = []
     for q in 제목:
-        if cnt < 3:
-            cnt += 1
+        if q.get_text()[-2] == ']':
+            # db['title'] = q.get_text()[1:-5]
+            title.append(q.get_text()[1:-5])
         else:
-            if q.get_text()[-2] == ']':
-                # db['title'] = q.get_text()[1:-5]
-                title.append(q.get_text()[1:-5])
-            else:
-                # db['title'] = q.get_text()[1:-2]
-                title.append(q.get_text()[1:-2])
-    # print(title)
+            # db['title'] = q.get_text()[1:-2]
+            title.append(q.get_text()[1:-2])
     # 글쓴이
     글쓴이 = soup.find_all(class_="nickname")
     writer = []
     for q in 글쓴이:
         # db['writer'] = q.get_text()
         writer.append(q.get_text())
-    # print(writer)
     # 작성일
-    cnt = 0
     작성일 = soup.find_all(class_="gall_date")
     date = []
     for q in 작성일:
-        if cnt < 2:
-            cnt += 1
-        else:
-            # db['date'] = q.get_text()
-            date.append(q.get_text())
-    # print(date)
+        # db['date'] = q.get_text()
+        date.append(q.get_text())
     # 조회수
-    cnt = 0
     조회수 = soup.find_all(class_="gall_count")
     count = []
     for q in 조회수:
-        if cnt < 2:
-            cnt += 1
-        else:
-            # db['count'] = q.get_text()
-            count.append(q.get_text())
-    # print(count)
+        # db['count'] = q.get_text()
+        count.append(q.get_text())
+    # 글 번호
+    글번호 = soup.find_all(class_="gall_num")
+    write_num = []
+    for q in 글번호:
+        write_num.append(q.get_text())
+    print(write_num)
+    # db만들기
+    k = len(title) - len(writer)  # 공지글 제외
     data = []
-    for i in range(len(title)):
+    for i in range(len(writer)):
         db = {}
-        db['title'] = title[i]
+        db['title'] = title[i+k]
         db['writer'] = writer[i]
-        db['date'] = date[i]
-        db['count'] = count[i]
+        db['date'] = date[i+k]
+        db['count'] = count[i+k]
+        db['id'] = str(int(write_num[k])-int(write_num[i+k]))
         data.append(db)
+    print(data)
+    print(write_num[-1])
     return data
 
 
--- daver/mailview/views.py
+++ daver/mailview/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .crawling import crawl
 from django.http import Http404
+from bs4 import BeautifulSoup as bs
+import requests
 
 
 def index(request):
     data = crawl()
-    # writer = crawling.writer
-    # date = crawling.date
-    # count = crawling.count
-    print(data)
     return render(request, 'mail_main.html', {'data': data})
 
 
 def mail_detail(request, pk):
     data = crawl()
-    try:
-        data = data[pk]
-    except data[pk] >= len(data):
+    print(len(data))
+    url = "https://m.dcinside.com/board/gongik_new/2329281"
+    header = {'User-agent': '*'}
+    html = requests.get(url, headers=header)
+    soup = bs(html.content, "html.parser")
+    #글내용 = soup.find_all(class_="gall_tit ub-word")
+    # print(글내용)
+    if pk >= len(data):
         raise Http404('게시글을 찾을 수 없습니다')
+    else:
+        data = data[pk]
     return render(request, 'mail_detail.html', {'data': data})
