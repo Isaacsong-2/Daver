c6d4b7d90bce017b1e9ace1d01298f90ac84d7d6
Isak Song <thddltkr10@naver.com>
2021-12-07 21:28:35 +0900

    이미지파일 깨짐현상 수정(css)

--- daver/mailview/views.py
+++ daver/mailview/views.py
@@ -13,19 +13,19 @@ def index(request):
 
 
 def mail_detail(request, pk):
-    data = crawl()[0][pk]
+    data = crawl()[0]
     url = ParseResult(scheme='https', netloc='gall.dcinside.com', path='/board/view/',
-                      params='', query='id=gongik_new&no=' + data['write_num'] + '&page=1', fragment='')
+                      params='', query='id=gongik_new&no=' + data[pk]['write_num'] + '&page=1', fragment='')
     url = url.geturl()  # url 생성
 
     header = {'User-agent': '*'}
     html = requests.get(url, headers=header)
     soup = bs(html.content, "html.parser")
     글내용 = soup.find_all(class_="write_div")  # 글내용 스크래핑
-    data['content'] = 글내용  # db에 추가
+    data[pk]['content'] = 글내용  # db에 추가
     print(글내용)
     if pk >= len(data):
         raise Http404('게시글을 찾을 수 없습니다')
     else:
-        data = data
+        data = data[pk]
     return render(request, 'mail_detail.html', {'data': data})
