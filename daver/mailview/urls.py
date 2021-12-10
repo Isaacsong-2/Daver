475da8ec62827b84c644c40ba1c18bc8cfb82566
Isak Song <thddltkr10@naver.com>
2021-12-10 20:38:22 +0900

    전체화면에서 이미지깨짐현상 수정중(왜깨지지)

--- daver/mailview/urls.py
+++ daver/mailview/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.mail),
     path('<int:pk>/', views.mail_detail),
 ]
--- daver/mailview/views.py
+++ daver/mailview/views.py
@@ -4,16 +4,17 @@ from .crawling import crawl
 from django.http import Http404
 from bs4 import BeautifulSoup as bs
 import requests
-from urllib.parse import ParseResult, urlparse, urlunparse
+from urllib.parse import ParseResult
 
 
-def index(request):
+def mail(request):
     data = crawl()[0]
     return render(request, 'mail_main.html', {'data': data})
 
 
 def mail_detail(request, pk):
     data = crawl()[0]
+    #data = saved().data
     url = ParseResult(scheme='https', netloc='gall.dcinside.com', path='/board/view/',
                       params='', query='id=gongik_new&no=' + data[pk]['write_num'] + '&page=1', fragment='')
     url = url.geturl()  # url 생성
@@ -23,7 +24,7 @@ def mail_detail(request, pk):
     soup = bs(html.content, "html.parser")
     글내용 = soup.find_all(class_="write_div")  # 글내용 스크래핑
     data[pk]['content'] = 글내용  # db에 추가
-    print(글내용)
+    # print(글내용)
     if pk >= len(data):
         raise Http404('게시글을 찾을 수 없습니다')
     else:
